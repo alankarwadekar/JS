fa56be2d504b0185da399b13ca6bf6f2caf086df
alankarwadekar <alankarwadekar2@gmail.com>
2024-12-13 08:25:41 +0000

    operators Completed

--- Practice/CH_02_Basics/Operator.js
+++ Practice/CH_02_Basics/Operator.js
@@ -1,4 +1,59 @@
 // Arithmetic Operators 
 //+,-,*,/,**,//,++,--
+// a=10
+// console.log(10+10)
+// console.log(10-10)
+// console.log(10*10)
+// console.log(100/10)
+// console.log(100%3)
+// console.log(a++)
+// console.log(a)
+// console.log(++a)
+// console.log(--a)
+// console.log(a--)
 
-console.log(10+10)
+// Relational / Comparison Operators 
+//<,>,<=,>=,==,!=,===,!==  (true/false)
+
+// console.log(10>5);
+// console.log(10<5);
+// console.log(10<=5);
+// console.log(10>=5);
+// console.log(10==10);
+// console.log(10=='10');
+// console.log(10==='10');
+// console.log(10===10);
+// console.log(10!=='10');
+// console.log(10!=100);
+
+// Logical Operators 
+// && || !
+
+// console.log(10>5 && 10<100);
+// console.log(10<5 || 10>100);
+// console.log(!(10>5&&10<500||10>10)); 
+
+// console.log(!false);
+
+
+//Assignment Operators 
+// =,+=,-=,*=,/=,...
+let a = 10;
+// a=20;
+// a=a+20;
+// a+=20;
+// a-=20;
+// console.log(a)
+
+
+// Bitwise Operators 
+// & | ^ ~ << >>
+
+// console.log(4&5);
+// console.log(4|5);
+// console.log(4^5);
+// console.log(~-10);
+// console.log(10<<2);
+// console.log(10>>2);
+
+
