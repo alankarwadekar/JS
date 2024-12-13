80c530d4445b0a799f4d1a7c153e4e1d397a1421
alankarwadekar <alankarwadekar2@gmail.com>
2024-12-13 07:40:10 +0000

    10/12/24

---
+++ Learning/ch 01 basics/variables.js
@@ -0,0 +1,16 @@
+// var score = 0
+// console.log (score);
+//  score = 123
+// console.log (score);
+
+
+var age = 21
+var temp = 25.6
+var name = "john"
+var ismale = true
+var score = "o";
+console.log( "age :", age);
+console.log( "temp :", temp);
+console.log( "name :",name);
+console.log( "ismale :", ismale);
+console.log("score :",score)
