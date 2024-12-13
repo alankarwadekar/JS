27f3df3b98121a2d5a496c9cc380b8c6eef02681
alankarwadekar <alankarwadekar2@gmail.com>
2024-12-13 07:48:01 +0000

    OPerator Started

--- Practice/CH_02_Basics/Operator.js
+++ Practice/CH_02_Basics/Operator.js
@@ -1,36 +1,4 @@
-// Arithmetic 
+// Arithmetic Operators 
+//+,-,*,/,**,//,++,--
 
-// let a =2;
-// let b= 3;
-
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a%b);
-// console.log(2**2);
-
-// Unary 
-
-// let a = 10;
-// console.log(++a);//pre increment
-// console.log(++a);//pre decrement
-// console.log(--a);
-// console.log(a++); //post increment
-// console.log(a);
-
-// Relational / Comparison 
-
-let a = 10;
-let b= 100;
-
-// console.log(a<b);
-// console.log(a>b);
-// console.log(a<=b);
-// console.log(a>=b);
-// console.log(a==b);//checks value only
-// console.log(a!=b);
-
-// console.log(10===10); // also check data type + value
-console.log(10==="10");
-
+console.log(10+10)
